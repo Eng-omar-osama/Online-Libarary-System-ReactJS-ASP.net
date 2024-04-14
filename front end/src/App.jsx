b8acc55b7d99f9e6df7cdc83f484c2a8d8e528b2
Salah Mohamed <salah.mohamed0404@gmail.com>
2024-04-14 15:50:39 +0200

    Update import aliases in App.jsx

--- front end/src/App.jsx
+++ front end/src/App.jsx
@@ -4,63 +4,68 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import { Toaster } from "react-hot-toast";
 
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-
-import HomeLayout from "./ui/HomeLayout";
-import NotFound from "./pages/NotFound";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import Home from "./pages/Home";
+
+import HomeLayout from "@ui/HomeLayout";
+import Home from "@pages/Home";
+import NotFound from "@pages/NotFound";
+import Login from "@pages/Login";
+import SignUp from "@pages/SignUp";
 
 function App() {
-    const queryClient = new QueryClient({
-        defaultOptions: {
-            queries: {
-                staleTime: 0,
-            },
-        },
-    });
+	const queryClient = new QueryClient({
+		defaultOptions: {
+			queries: {
+				staleTime: 0,
+			},
+		},
+	});
 
-    return (
-        <>
-            <QueryClientProvider client={queryClient}>
-                <ReactQueryDevtools initialIsOpen={false} />
+	return (
+		<>
+			<QueryClientProvider client={queryClient}>
+				<ReactQueryDevtools initialIsOpen={false} />
 
-                <BrowserRouter>
-                    <Routes>
-                        <Route element={<HomeLayout />}>
-                            <Route path="/" element={<Home />} />
-                        </Route>
+				<BrowserRouter>
+					<Routes>
+						<Route element={<HomeLayout />}>
+							<Route path="/" element={<Home />} />
+						</Route>
 
-                        {/** admin routes **/}
+						<Route path="/login" element={<Login />} />
+						<Route path="/signup" element={<SignUp />} />
 
-                        <Route path="*" element={<NotFound />} />
-                    </Routes>
-                </BrowserRouter>
+						{/** admin routes **/}
 
-                <Toaster
-                    gutter={12}
-                    position="top-center"
-                    containerStyle={{ margin: "0px" }}
-                    toastOptions={{
-                        success: {
-                            duration: 3000,
-                        },
-                        error: {
-                            duration: 5000,
-                        },
+						<Route path="*" element={<NotFound />} />
+					</Routes>
+				</BrowserRouter>
 
-                        style: {
-                            fontSize: "16px",
-                            maxWidth: "500px",
-                            padding: "16px 24px",
-                            backgroundColor: "var(--color-grey-0)",
-                            color: "var(--color-grey-700)",
-                        },
-                    }}
-                />
-            </QueryClientProvider>
-        </>
-    );
+				<Toaster
+					gutter={12}
+					position="top-center"
+					containerStyle={{ margin: "0px" }}
+					toastOptions={{
+						success: {
+							duration: 3000,
+						},
+						error: {
+							duration: 5000,
+						},
+
+						style: {
+							fontSize: "16px",
+							maxWidth: "500px",
+							padding: "16px 24px",
+							backgroundColor: "var(--color-grey-0)",
+							color: "var(--color-grey-700)",
+						},
+					}}
+				/>
+			</QueryClientProvider>
+		</>
+	);
 }
 
 export default App;
