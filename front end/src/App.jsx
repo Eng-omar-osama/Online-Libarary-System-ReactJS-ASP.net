d853e8af3bee863d5d3156232d444417c9264721
Ali_Alaa_Elidn <alialaa932003@gmail.com>
2024-04-16 11:02:02 +0200

    OLS-12 modify the conflict

--- front end/src/App.jsx
+++ front end/src/App.jsx
@@ -7,8 +7,7 @@ import { Toaster } from "react-hot-toast";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
- BookDetails from "./pages/BookDetails";
-
+import BookDetails from "./pages/BookDetails";
 
 import HomeLayout from "@ui/HomeLayout";
 import Home from "@pages/Home";
@@ -16,27 +15,19 @@ import NotFound from "@pages/NotFound";
 import Login from "@pages/Login";
 import SignUp from "@pages/SignUp";
 
-
 function App() {
-	const queryClient = new QueryClient({
-		defaultOptions: {
-			queries: {
-				staleTime: 0,
-			},
-		},
-	});
+    const queryClient = new QueryClient({
+        defaultOptions: {
+            queries: {
+                staleTime: 0,
+            },
+        },
+    });
 
-	return (
-		<>
-			<QueryClientProvider client={queryClient}>
-				<ReactQueryDevtools initialIsOpen={false} />
-
-				<BrowserRouter>
-					<Routes>
-						<Route element={<HomeLayout />}>
-							<Route path="/" element={<Home />} />
-						</Route>
-
+    return (
+        <>
+            <QueryClientProvider client={queryClient}>
+                <ReactQueryDevtools initialIsOpen={false} />
 
                 <BrowserRouter>
                     <Routes>
@@ -48,40 +39,39 @@ function App() {
                             />
                         </Route>
 
-						<Route path="/login" element={<Login />} />
-						<Route path="/signup" element={<SignUp />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/signup" element={<SignUp />} />
 
+                        {/** admin routes **/}
 
-						{/** admin routes **/}
+                        <Route path="*" element={<NotFound />} />
+                    </Routes>
+                </BrowserRouter>
 
-						<Route path="*" element={<NotFound />} />
-					</Routes>
-				</BrowserRouter>
+                <Toaster
+                    gutter={12}
+                    position="top-center"
+                    containerStyle={{ margin: "0px" }}
+                    toastOptions={{
+                        success: {
+                            duration: 3000,
+                        },
+                        error: {
+                            duration: 5000,
+                        },
 
-				<Toaster
-					gutter={12}
-					position="top-center"
-					containerStyle={{ margin: "0px" }}
-					toastOptions={{
-						success: {
-							duration: 3000,
-						},
-						error: {
-							duration: 5000,
-						},
-
-						style: {
-							fontSize: "16px",
-							maxWidth: "500px",
-							padding: "16px 24px",
-							backgroundColor: "var(--color-grey-0)",
-							color: "var(--color-grey-700)",
-						},
-					}}
-				/>
-			</QueryClientProvider>
-		</>
-	);
+                        style: {
+                            fontSize: "16px",
+                            maxWidth: "500px",
+                            padding: "16px 24px",
+                            backgroundColor: "var(--color-grey-0)",
+                            color: "var(--color-grey-700)",
+                        },
+                    }}
+                />
+            </QueryClientProvider>
+        </>
+    );
 }
 
 export default App;
